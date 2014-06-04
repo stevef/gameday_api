153042379d34a953da4814d71cd3c7258eaf54c2
Steve Finkelstein <sf@stevefink.net>
2014-06-04 15:17:53 -0400

    correct how we retrieve stadium_id
    Signed-off-by: Steve Finkelstein <sf@stevefink.net>

--- lib/gameday_api/game.rb
+++ lib/gameday_api/game.rb
@@ -45,7 +45,7 @@ module GamedayApi
           @time = @xml_doc.root.attributes["local_game_time"]     
           info = GamedayUtil.parse_gameday_id('gid_'+gid)
           @home_team_abbrev = info["home_team_abbrev"]
-          @stadium_id = @xml_doc.root.elements['stadium/@id']
+          @stadium_id = @xml_doc.root.elements['stadium'].attributes['id']
           @visit_team_abbrev = info["visiting_team_abbrev"]
           @visiting_team = Team.new(@visit_team_abbrev )
           @home_team = Team.new(@home_team_abbrev )
