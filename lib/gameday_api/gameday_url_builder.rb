c3a1ca25cabb1f9759b657f2cb921ec0e24946c6
Ian Andersen <ianderse@mac.com>
2014-06-21 10:20:36 -0600

    fix (hopefully) all of the implicit conversion of fixnum to string errors (ruby 2.1.2)

--- lib/gameday_api/gameday_url_builder.rb
+++ lib/gameday_api/gameday_url_builder.rb
@@ -7,73 +7,73 @@ module GamedayApi
   
     def self.build_game_base_url(gid)
       gameday_info = GamedayUtil.parse_gameday_id('gid_' + gid)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + gameday_info['year'] + "/month_" + gameday_info['month'] + "/day_" + gameday_info['day'] + "/gid_"+gid 
+      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + gameday_info['year'] + "/month_" + gameday_info['month'] + "/day_" + gameday_info['day'] + "/gid_"+gid.to_s 
     end
   
   
     def self.build_eventlog_url(year, month, day, gid)
       set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/gid_"+gid+"/eventLog.xml" 
+      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year.to_s + "/month_" + @@month.to_s + "/day_" + @@day.to_s + "/gid_"+gid.to_s+"/eventLog.xml" 
     end
   
   
     def self.build_epg_url(year, month, day)
       set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/epg.xml"
+      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year.to_s + "/month_" + @@month.to_s + "/day_" + @@day.to_s + "/epg.xml"
     end
   
   
     def self.build_scoreboard_url(year, month, day)
       set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/master_scoreboard.xml"
+      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year.to_s + "/month_" + @@month.to_s + "/day_" + @@day.to_s + "/master_scoreboard.xml"
     end
   
   
     def self.build_day_highlights_url(year, month, day)
       set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/media/highlights.xml"
+      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year.to_s + "/month_" + @@month.to_s + "/day_" + @@day.to_s + "/media/highlights.xml"
     end
   
   
     def self.build_boxscore_url(year, month, day, gid)
       set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/gid_"+gid+"/boxscore.xml" 
+      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year.to_s + "/month_" + @@month.to_s + "/day_" + @@day.to_s + "/gid_"+gid.to_s+"/boxscore.xml" 
     end
   
   
     def self.build_game_url(year, month, day, gid)
       set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/gid_"+gid+"/game.xml" 
+      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year.to_s + "/month_" + @@month.to_s + "/day_" + @@day.to_s + "/gid_"+gid.to_s+"/game.xml" 
     end
   
   
     def self.build_game_events_url(year, month, day, gid)
       set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/gid_"+gid+"/game_events.xml" 
+      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year.to_s + "/month_" + @@month.to_s + "/day_" + @@day.to_s + "/gid_"+gid.to_s+"/game_events.xml" 
     end
   
   
     def self.build_gamecenter_url(year, month, day, gid)
       set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/gid_"+gid+"/gamecenter.xml" 
+      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year.to_s + "/month_" + @@month.to_s + "/day_" + @@day.to_s + "/gid_"+gid.to_s+"/gamecenter.xml" 
     end
 
 
     def self.build_linescore_url(year, month, day, gid)
       set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/gid_"+gid+"/linescore.xml" 
+      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year.to_s + "/month_" + @@month.to_s + "/day_" + @@day.to_s + "/gid_"+gid.to_s+"/linescore.xml" 
     end
   
 
     def self.build_players_url(year, month, day, gid)
       set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/gid_"+gid+"/players.xml" 
+      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year.to_s + "/month_" + @@month.to_s + "/day_" + @@day.to_s + "/gid_"+gid.to_s+"/players.xml" 
     end
   
   
     def self.build_batter_url(year, month, day, gid, pid)
       set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/gid_"+gid+"/batters/" +  pid + '.xml'
+      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year.to_s + "/month_" + @@month.to_s + "/day_" + @@day.to_s + "/gid_"+gid.to_s+"/batters/" +  pid.to_s + '.xml'
     end
   
   
@@ -90,19 +90,19 @@ module GamedayApi
   
     def self.build_inningx_url(year, month, day, gid, inning_num)
       set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/gid_"+gid+"/inning/inning_#{inning_num}.xml"
+      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year.to_s + "/month_" + @@month.to_s + "/day_" + @@day.to_s + "/gid_"+gid.to_s+"/inning/inning_#{inning_num}.xml"
     end
   
   
     def self.build_inning_scores_url(year, month, day, gid)
       set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/gid_"+gid+"/inning/inning_Scores.xml"
+      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year.to_s + "/month_" + @@month.to_s + "/day_" + @@day.to_s + "/gid_"+gid.to_s+"/inning/inning_Scores.xml"
     end
   
   
     def self.build_inning_hit_url(year, month, day, gid)
       set_date_vars(year, month, day)
-      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year + "/month_" + @@month + "/day_" + @@day + "/gid_"+gid+"/inning/inning_hit.xml"
+      "#{Gameday::GD2_MLB_BASE}/mlb/year_" + @@year.to_s + "/month_" + @@month.to_s + "/day_" + @@day.to_s + "/gid_"+gid.to_s+"/inning/inning_hit.xml"
     end
   
 
