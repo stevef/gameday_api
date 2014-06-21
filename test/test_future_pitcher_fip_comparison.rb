bc7c18d8189e8824014a2d8c4c6f66fe7eb9e97f
Ian Andersen <ianderse@mac.com>
2014-06-21 10:48:44 -0600

    better test

--- test/test_future_pitcher_fip_comparison.rb
+++ test/test_future_pitcher_fip_comparison.rb
@@ -4,10 +4,22 @@ require '../lib/gameday_api/futurelinescore'
 require '../lib/gameday_api/pitcher'
 
 game = GamedayApi::FutureLineScore.new
-pitcher = GamedayApi::Pitcher.new
+pitcher_home = GamedayApi::Pitcher.new
+pitcher_away = GamedayApi::Pitcher.new
 
 game.load_from_id('2014_06_21_tormlb_cinmlb_1')
 
 pitcherid = game.home_pitcher.pid
+pitcher_away_id = game.away_pitcher.pid
 
-puts pitcher.load_from_year_id(2014, 502190)
+pitcher_home.load_from_year_id(2014, pitcherid)
+pitcher_away.load_from_year_id(2014, pitcher_away_id)
+
+puts pitcher_home.fip
+puts pitcher_away.fip
+
+if pitcher_home.fip > pitcher_away.fip
+	puts "Away pitcher has the advantage"
+else
+	puts "Home pitcher has the advantage"
+end
